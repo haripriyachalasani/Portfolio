e72e121775f3cfc3bae537c2671e8c2ffd546a31
Hari Priya Chalasani <priyachalasani1@gmail.com>
2024-08-06 21:04:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+
+  $("#ajaxForm").submit(function(e) {
+    e.preventDefault();
+    var action = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      url: action,
+      crossDomain: true,
+      data: new FormData(this),
+      dataType: "json",
+      processData: false,
+      contentType: false,
+      headers: {
+        "Accept": "application/json"
+      }
+    }).done(function() {
+      window.location.href = "https://thankyoumsg.netlify.app/";
+    }).fail(function() {
+      alert('An error occurred! Please try again later.');
+    });
+  });
+
